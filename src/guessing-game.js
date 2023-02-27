a7bc41fb9ee63f13bf921e60a19723581cd3114c
ZakirovInsaf <sadsquire@gmail.com>
2023-02-27 15:59:33 +0300

    final: task is done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.min = 0;
+    this.max = 0;
+    this.center = 0;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    this.center = Math.round((this.max - this.min) / 2) + this.min;
+    return this.center;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.center;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.center;
+  }
 }
 
 module.exports = GuessingGame;
